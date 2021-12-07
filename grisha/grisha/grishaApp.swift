491bddce17723381840607fe33439ed3eb188ef0
newzelend <89086288+newzelend@users.noreply.github.com>
2021-12-07 15:31:15 +0300

    main

---
+++ grisha/grisha/grishaApp.swift
@@ -0,0 +1,17 @@
+//
+//  grishaApp.swift
+//  grisha
+//
+//  Created by Grisha Pospelov on 07.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct grishaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
